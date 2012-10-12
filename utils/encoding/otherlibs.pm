b047a43bd57ae676624d1427cc83a398cacf2020
Dee Newcum <dee.newcum@gmail.com>
2012-10-12 18:14:30 -0500

    .

---
+++ utils/encoding/otherlibs.pm
@@ -0,0 +1,10 @@
+
+
+# Use the libs from the .../src/ directory.
+
+# We put this in one place, so that if we have to change the path, we only have to do it in one
+# file, instead of ten.
+
+use lib '../../src';
+
+1;
