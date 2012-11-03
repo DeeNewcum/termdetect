b7c60bc72dec28bf9110c2555c3f11e1d9416559
Dee Newcum <dee.newcum@gmail.com>
2012-11-02 20:57:11 -0500

    bugfix: when empty termmatch, give better error

--- src/Termdetect_Match.pm
+++ src/Termdetect_Match.pm
@@ -61,12 +61,16 @@ sub match_results {
         }
     } elsif (@no_mismatches == 0) {
         if ($die_if_not_one) {
-            print STDERR "Error: No terminals matched exactly.  Terminals that were close:\n";
-            my $closest_match = show_match_percentages($match_stats, 5);
-            print STDERR "\nRun this for more info:\n    $0 --check=$closest_match\n";
-            print STDERR "\n\nIt would help out greatly if you could submit data about this.\n";
-            print STDERR "See       termdetect --help-submit\n";
-            exit 1;
+            if (keys %$match_stats) {
+                print STDERR "Error: No terminals matched exactly.  Terminals that were close:\n";
+                my $closest_match = show_match_percentages($match_stats, 5);
+                print STDERR "\nRun this for more info:\n    $0 --check=$closest_match\n";
+                print STDERR "\n\nIt would help out greatly if you could submit data about this.\n";
+                print STDERR "See       termdetect --help-submit\n";
+                exit 1;
+            } else {
+                die "Error: Termmatch.src is blank; there are no entries to match to.\n";
+            }
         }
     } elsif (@no_mismatches == 1) {
         Termdetect_Tests::calculate_derived_values_after_match($test_results, $termmatch_db, $highest_match);
